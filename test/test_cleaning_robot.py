071e93788547b6df6ab1af0d03f16b7f06a741cf
Taranum67 <taranumnisha700@gmail.com>
2024-12-05 00:22:21 +0100

    User Story 1

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -8,9 +8,28 @@ from src.cleaning_robot import CleaningRobot
 
 class TestCleaningRobot(TestCase):
 
-    @patch.object(GPIO, "input")
-    def test_something(self, mock_object: Mock):
-        # This is an example of test where I want to mock the GPIO.input() function
-        pass
+    #User Story 1
+    def test_initialize_robot(self):
+        robot = CleaningRobot()
+        robot.initialize_robot()
+        self.assertEqual(robot.pos_x, 0)
+        self.assertEqual(robot.pos_y, 0)
+        self.assertEqual(robot.heading, 'N')
+
+    def test_robot_status(self):
+        robot = CleaningRobot()
+        robot.initialize_robot()
+        status = robot.robot_status()
+        self.assertEqual(status, "(0,0,N)")
+
+    def test_robot_status_after_update(self):
+        robot = CleaningRobot()
+        robot.initialize_robot()
+        robot.pos_x = 2
+        robot.pos_y = 3
+        robot.heading = 'S'
+        status = robot.robot_status()
+        self.assertEqual(status, "(2,3,S)")
+
 
 

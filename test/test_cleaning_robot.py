429b3f4496bdcbb1a236c0dafb9cd34434a855cf
Taranum67 <taranumnisha700@gmail.com>
2024-12-23 18:41:05 +0100

    RED PHASE US2-2

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -22,3 +22,11 @@ class TestCleaningRobot(TestCase):
         mock_ibs.return_value = 20
         system.manage_cleaning_system()
         mock_led.assert_has_calls([call(system.RECHARGE_LED_PIN, GPIO.LOW), call(system.CLEANING_SYSTEM_PIN, GPIO.HIGH)])
+
+    @patch.object(IBS, "get_charge_left")
+    @patch.object(GPIO, "output")
+    def test_manage_cleaning_system_below_10(self, mock_led: Mock, mock_ibs: Mock):
+        system = CleaningRobot()
+        mock_ibs.return_value = 7
+        system.manage_cleaning_system()
+        mock_led.assert_has_calls([call(system.RECHARGE_LED_PIN, GPIO.HIGH), call(system.CLEANING_SYSTEM_PIN, GPIO.LOW)])
